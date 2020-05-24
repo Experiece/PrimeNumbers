29fa70ae6f10b16e3d3e093c87a71aca67c6e1b1
Forgoot <forgoot@mail.ru>
2020-05-24 17:26:36 +0300

    I god damn did it!!!!! This bitch Works!

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -2,18 +2,24 @@ import java.util.ArrayList;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-//        ArrayList<Integer> primeNumbersArr = new ArrayList<>();
+        ArrayList<Integer> primeNumbersArr = new ArrayList<>();
 
-        for (int potentialPrimeNumber = 2; potentialPrimeNumber < 20;) {
+        for (int potentialPrimeNumber = 1; potentialPrimeNumber < 1000; ) {
             potentialPrimeNumber++;
-            for (int q = 2; q < potentialPrimeNumber;) {
+            for (int q = 2; q <= potentialPrimeNumber; ) {
                 int ost;
                 ost = potentialPrimeNumber % q;
                 if (ost != 0) {
                     q++;
                 } else {
-                    System.out.println(potentialPrimeNumber);
-                    break;
+                    if (potentialPrimeNumber != q) {
+                        break;
+                    }
+                    else {
+                        System.out.println(potentialPrimeNumber);
+                        primeNumbersArr.add(potentialPrimeNumber);
+                        break;
+                    }
                 }
             }
         }
