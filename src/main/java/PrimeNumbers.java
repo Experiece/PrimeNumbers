5a63efe57121a8b38ee3770edceb88a1e5bad21c
Maksim Andruhin <maksim.andruhin@gmail.com>
2020-05-24 20:24:05 +0300

    Refactoring logic

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -7,38 +7,31 @@ public class PrimeNumbers {
     public static void main(String[] args) throws IOException {
         ArrayList<Integer> primeNumbersArr = new ArrayList<>();
 
-        for (int potentialPrimeNumber = 1; potentialPrimeNumber < 100; ) {
-            potentialPrimeNumber++;
-            for (int q = 2; q <= potentialPrimeNumber; ) {
-                int ost;
-                ost = potentialPrimeNumber % q;
-                if (ost != 0) {
-                    q++;
-                } else {
-                    if (potentialPrimeNumber != q) {
-                        break;
-                    } else {
-                        //                       System.out.println(potentialPrimeNumber);
-                        primeNumbersArr.add(potentialPrimeNumber);
-                        break;
-                    }
+        for (int potentialPrimeNumber = 2; potentialPrimeNumber < 100; potentialPrimeNumber++) {
+            boolean isPrime = true;
+            for (int q = 2; q < potentialPrimeNumber; q++) {
+                int ost = potentialPrimeNumber % q;
+                if (ost == 0) {
+                    isPrime = false;
+                    break;
                 }
             }
+
+            if (isPrime) {
+                primeNumbersArr.add(potentialPrimeNumber);
+            }
         }
+
         int n = 97;
-        int indexNumber;
-        int value;
 
-        for (indexNumber = 0; indexNumber < primeNumbersArr.size(); ) {
+        for (int indexNumber = 0; indexNumber < primeNumbersArr.size(); indexNumber++) {
 
-            value = n % primeNumbersArr.get(indexNumber);
+            int value = n % primeNumbersArr.get(indexNumber);
             if (value == 0) {
                 System.out.println(primeNumbersArr.get(indexNumber));
                 n = n / primeNumbersArr.get(indexNumber);
-            } else {
-                indexNumber++;
+                indexNumber--;
             }
-
         }
     }
 }
